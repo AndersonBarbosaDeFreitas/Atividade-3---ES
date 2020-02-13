381571e8e7e334b1135d8600f14c4a72fb1ee774
joana.darck <joana.darck@LAB2-DCX-09.labs.dcx>
2020-02-13 08:31:55 -0300

    print data de hoje

--- Teste/src/test/br/test.java
+++ Teste/src/test/br/test.java
@@ -1,7 +1,11 @@
 package test.br;
 
 public class test {
-	public static void main(String args[]) {
+	
+	public static void main(String args[]) {	
 		System.out.println("Finalmente Consegui!");
+		System.out.println("13 de fevereiro de 2020");
+	
 	}
+	
 }
